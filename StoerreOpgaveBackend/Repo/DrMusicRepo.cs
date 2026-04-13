04cd61592c4c392a70ef973c66d52633336dbf58
Fulle4600 <easyhjemmeside@gmail.com>
2026-04-13 11:41:34 +0200

    repo list er done

---
+++ StoerreOpgaveBackend/Repo/DrMusicRepo.cs
@@ -0,0 +1,56 @@
+﻿using StoerreOpgaveBackend.models;
+namespace StoerreOpgaveBackend.Repo
+{
+    public class DrMusicRepo
+    {
+        private readonly List<DrMusic> musics = new();
+        private int nextId = 1;
+        
+        public DrMusic Add(string Title, int Id, string Artist, int Duration, int PublicationDate)
+            {
+            var music = new DrMusic
+            {
+                Id = nextId++,
+                title = Title,
+                artist = Artist,
+                duration = Duration,
+                publicationDate = PublicationDate
+            };
+            musics.Add(music);
+            return music;
+        }
+        public List<DrMusic> GetAll()
+        {
+            return musics;
+        }
+        public DrMusic? GetById(int id)
+        {
+            return musics.FirstOrDefault(m => m.Id == id);
+        }
+         public bool Delete(int id)
+        {
+            var music = GetById(id);
+            if (music != null)
+            {
+                musics.Remove(music);
+                return true;
+            }
+            return false;
+
+        }
+        public DrMusic? Update(int id, string Title, string Artist, int Duration, int PublicationDate)
+        {
+            var music = GetById(id);
+            if (music != null)
+            {
+                music.title = Title;
+                music.artist = Artist;
+                music.duration = Duration;
+                music.publicationDate = PublicationDate;
+                return music;
+            }
+            return null;
+        }
+
+    }
+}
