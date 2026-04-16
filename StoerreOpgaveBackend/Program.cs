080e83416f28e7fdc3a3ab9bbb7d37d8cce3de79
Fulle4600 <easyhjemmeside@gmail.com>
2026-04-16 15:11:08 +0200

    .

--- StoerreOpgaveBackend/Program.cs
+++ StoerreOpgaveBackend/Program.cs
@@ -4,11 +4,14 @@ using StoerreOpgaveBackend.Repo;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-      options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<dbDrMusicRepo>();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddCors(option =>
 {
     option.AddPolicy("AllowAll", policy =>
